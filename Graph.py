e985a3498c697988e81fd37a0a6ac2f4c511c75a
Dobrojan Alexandru <alexandru.dobrojan@corlab.tech>
2023-05-15 15:54:49 +0300

    lab-7

--- Graph.py
+++ Graph.py
@@ -1,159 +1,67 @@
-import random
-import sys
-import networkx as nx
-import matplotlib.pyplot as plt
-
-INF = sys.maxsize
-MIN = 1
-MAX = 100
+import heapq
+from collections import *
+from profiler import *
 
 
 class Graph:
-	_g: list[list[int]] = []
-	size: int = 0
-	edges_count: int = 0
+	def __init__(self, vertices: int):
+		self.vertices = vertices
+		self.adj_list = {vertex: [] for vertex in range(self.vertices)}
 
-	def __init__(self, size: int):
-		self.size = size
+	def add_edge(self, u, v, weight):
+		self.adj_list[u].append((v, weight))
+		self.adj_list[v].append((u, weight))
 
-		for i in range(size):
-			ls = []
-			for j in range(size):
-				ls += [INF]
-			self._g += [ls]
 
-	def add(self, v1: int, v2: int, value: int):
-		if v1 == v2:
-			return
+def find(parent, i):
+	if parent[i] == i:
+		return i
+	return find(parent, parent[i])
 
-		self._g[v1][v2] = value
-		self._g[v2][v1] = value
 
-	def get(self, v1: int, v2: int):
-		return self._g[v1][v2]
+@exec_time("kruskal")
+def kruskal(graph, start):
+	result = []  # This will store the resultant MST
+	edges = []
 
-	def is_connected(self, v1: int, v2: int):
-		return self.get(v1, v2) != INF
+	# Create a list of all edges in the graph
+	for node in graph.adj_list:
+		for neighbor, weight in graph.adj_list[node]:
+			edges.append((weight, node, neighbor))
 
-	def populate_complete(self, min: int = MIN, max: int = MAX):
-		self.edges_count = self.get_max_edges()
+	# Sort all the edges in non-decreasing order of their weight
+	edges.sort()
 
-		for i in range(self.size - 1):
-			for j in range(self.size - 1):
-				self.add(i, j, random.randint(min, max))
+	parent = [i for i in range(graph.vertices)]
 
-	def populate(self, connections_count: int, min: int = MIN, max: int = MAX):
-		self.edges_count = connections_count
+	for edge in edges:
+		weight, u, v = edge
+		parent_u = find(parent, u)
+		parent_v = find(parent, v)
+		if parent_u != parent_v:
+			result.append((u, v, weight))
+			parent[parent_u] = parent_v
 
-		if connections_count > self.get_max_edges():
-			raise Exception(f"Too many connections\nGot {connections_count}\nMax: {self.get_max_edges()}\nSize: {self.size}")
+	return result
 
-		for i in range(connections_count):
-			v1 = random.randint(0, self.size - 1)
-			v2 = random.randint(0, self.size - 1)
 
-			while v1 == v2:
-				v1 = random.randint(0, self.size - 1)
-				v2 = random.randint(0, self.size - 1)
+@exec_time("prim")
+def prim(graph, start_vertex):
+	mst = defaultdict(set)
+	visited = {start_vertex}
+	edges = [
+		(cost, start_vertex, to)
+		for to, cost in graph.adj_list[start_vertex]
+	]
+	heapq.heapify(edges)
 
-			self.add(v1, v2, random.randint(min, max))
+	while edges:
+		cost, frm, to = heapq.heappop(edges)
+		if to not in visited:
+			visited.add(to)
+			mst[frm].add(to)
+			for to_next, cost2 in graph.adj_list[to]:
+				if to_next not in visited:
+					heapq.heappush(edges, (cost2, to, to_next))
 
-	def print(self):
-		for i in range(self.size):
-			string = ''
-
-			for j in range(self.size):
-				string += self.get(i, j).__str__() + ' '
-
-			print(string)
-
-	def show(self):
-		plt.figure(figsize=(8, 6))
-		G = nx.Graph()
-
-		for i in range(self.size):
-			for j in range(self.size):
-				if i != j and self.is_connected(i, j):
-					G.add_edge(i, j)
-
-		nx.draw(
-			G, node_size=1000,
-			labels={node: node for node in G.nodes()}
-		)
-
-		plt.savefig('Graph.png')
-		plt.show()
-
-	def get_max_edges(self) -> int:
-		return int((self.size * (self.size - 1)) / 2)
-
-	def dijkstra(self, v1: int, v2: int) -> list[int]:
-		dist = [INF] * self.size
-		visited = [False] * self.size
-		dist[v1] = 0
-
-		for i in range(self.size):
-			u = self._min_distance(dist, visited)
-			visited[u] = True
-
-			for v in range(self.size):
-				if not visited[v] and self._g[u][v] != INF and \
-					dist[u] + self._g[u][v] < dist[v]:
-					dist[v] = dist[u] + self._g[u][v]
-
-		path = [v2]
-		while path[-1] != v1:
-			for v in range(self.size):
-				if self._g[path[-1]][v] != INF and \
-					dist[v] + self._g[path[-1]][v] == dist[path[-1]]:
-					path.append(v)
-					break
-
-		return path[::-1]
-
-	def _min_distance(self, dist: list[int], visited: list[bool]) -> int:
-		min_dist = INF
-		min_vertex = -1
-
-		for v in range(self.size):
-			if not visited[v] and dist[v] <= min_dist:
-				min_dist = dist[v]
-				min_vertex = v
-
-		return min_vertex
-
-	def floyd_warshall(self, v1: int, v2: int) -> list[int]:
-		dist = [[INF if self._g[i][j] == INF else self._g[i][j] for j in range(self.size)] for i in range(self.size)]
-		next = [[j if self._g[i][j] != INF else None for j in range(self.size)] for i in range(self.size)]
-
-		for k in range(self.size):
-			for i in range(self.size):
-				for j in range(self.size):
-					if dist[i][k] == INF or dist[k][j] == INF:
-						continue
-
-					new_dist = dist[i][k] + dist[k][j]
-					if new_dist < dist[i][j]:
-						dist[i][j] = new_dist
-						next[i][j] = next[i][k]
-
-		if next[v1][v2] is None:
-			return []
-
-		path = [v1]
-		while path[-1] != v2:
-			path.append(next[path[-1]][v2])
-
-		return path
-
-	def get_density(self) -> float:
-		return self.edges_count / self.get_max_edges()
-
-	def is_sparse(self):
-		return self.get_density() < 0.5
-
-	def is_dense(self):
-		return self.get_density() >= 0.5
-
-	def is_complete(self):
-		return self.get_density() == 1
+	return mst
--- main.py
+++ main.py
@@ -1,40 +1,26 @@
-from datetime import datetime
-from Graph import Graph
+from profiler import *
+from Graph import *
+import random
 
-TESTS = 10
-SIZE = 100
-EDGES = 3000
-PATH_NODES = 3
-d_total_time = 0
-fw_total_time = 0
 
-g = Graph(SIZE)
+def generate_random_graph(N, p=0.5, min_weight=1, max_weight=15):
+	graph = Graph(N)
+	for i in range(N):
+		for j in range(i + 1, N):  # avoid self-loop and duplicate edges
+			if random.random() < p:  # if random number is less than p, create edge
+				weight = random.randint(min_weight, max_weight)
+				graph.add_edge(i, j, weight)
+	return graph
 
-for _ in range(TESTS):
-	g = Graph(SIZE)
-	g.populate(EDGES)
 
-	start = datetime.now()
-	for i in range(PATH_NODES):
-		g.dijkstra(0, i)
+inp = []
 
-	end = datetime.now()
+for n in range(10, 300, 10):
+	print("---")
+	print(n)
+	graph = generate_random_graph(n)
+	kruskal(graph, 0)
+	prim(graph, 0)
+	inp.append(n)
 
-	d_total_time += (end - start).microseconds
-
-	start = datetime.now()
-	for i in range(PATH_NODES):
-		g.floyd_warshall(0, i)
-
-	end = datetime.now()
-
-	fw_total_time += (end - start).microseconds
-
-print(
-	f'''Average time for a graph with {SIZE} nodes and {EDGES} edges \
-({'Complete' if g.is_complete() else 'Sparse' if g.is_sparse() else 'Dense'} with {g.get_density():.2f} density)
-Dijkstra: {d_total_time / TESTS:.2f}μs
-Floyd-Warshall: {fw_total_time / TESTS:.2f}μs
-Ratio: {d_total_time / fw_total_time:.4f} (D/FW) or {fw_total_time / d_total_time:.4f} (FW/D)
-In {TESTS} tests with shortest path between {PATH_NODES} random node pairs.'''
-)
+plot_result(inp)
